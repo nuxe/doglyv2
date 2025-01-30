61b18d008966cb969b60e5bde1d53e5b60e7d598
kush <kush2005@gmail.com>
2025-01-30 15:08:26 -0500

    Initial Commit

---
+++ DoglyV2UITests/DoglyV2UITestsLaunchTests.swift
@@ -0,0 +1,33 @@
+//
+//  DoglyV2UITestsLaunchTests.swift
+//  DoglyV2UITests
+//
+//  Created by Kush Agrawal on 1/30/25.
+//
+
+import XCTest
+
+final class DoglyV2UITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    @MainActor
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
