0846d03e7e2b48d84800b340215db090cff2dff3
kush <kush2005@gmail.com>
2025-02-09 17:58:22 -0500

    Hello world in SwiftUI

---
+++ DoglyV2/Modules/AppStart/ContentView.swift
@@ -0,0 +1,16 @@
+//
+//  ContentView.swift
+//  DoglyV2
+//
+//  Created by Kush Agrawal on 2/9/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, SwiftUI!")
+            .padding()
+            .font(.largeTitle)
+    }
+}
---
+++ DoglyV2/Modules/AppStart/MyApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyApp.swift
+//  DoglyV2
+//
+//  Created by Kush Agrawal on 2/9/25.
+//
+
+import SwiftUI
+
+@main
+struct MyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
